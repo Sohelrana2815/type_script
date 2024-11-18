52502718c568a12d7611c80b9ad71b869fbe0a05
Sohelrana2815 <sohel152302@gmail.com>
2024-11-18 14:50:24 +0600

    ts data type

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // step 1 : js file k compile korbo
 // step 2 : command tsc index.ts => index.js => node index.js
+// step 3 : ekbare compile korbo barbar na
 var userName = "SOhel Rana";
-var age = 25;
 console.log(userName);
-console.log(age);
+var age = 45;
+var price = 55;
+console.log(age, price);
--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
 // step 1 : js file k compile korbo
 // step 2 : command tsc index.ts => index.js => node index.js
+// step 3 : ekbare compile korbo barbar na
 
 let userName = "SOhel Rana";
-
-let age = 25;
-
 console.log(userName);
-console.log(age);
 
+
+let age = 45;
+let price = 55;
+console.log(age,price);
