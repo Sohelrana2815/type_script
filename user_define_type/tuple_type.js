4e550289b403ecf9660a96c943b4673cebeffd05
Sohelrana2815 <sohel152302@gmail.com>
2024-11-18 18:21:47 +0600

    tuple type

---
+++ user_define_type/tuple_type.js
@@ -0,0 +1,7 @@
+// mixed data - key, value pare
+// amra chaile push korte pari mane vetore notun value dhukai e dei
+var user;
+user = [101, "moon"];
+user.push(102, "rana");
+console.log(user);
+console.log(user[1]);
