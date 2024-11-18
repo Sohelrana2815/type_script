ea28c2615953b19bca0ab5bb35ccb5c2d9227a21
Sohelrana2815 <sohel152302@gmail.com>
2024-11-18 15:32:43 +0600

    i learn about some of important data type

---
+++ built_in_type/index.js
@@ -0,0 +1,11 @@
+// built_in data type: number,string,boolean, void, undefine, null
+var userId;
+var firstName;
+var lastName;
+var fullName;
+var isActivated;
+fullName;
+userId = 101;
+firstName = "Sohel";
+lastName = "Rana";
+isActivated = true;
---
+++ built_in_type/index.ts
@@ -0,0 +1,15 @@
+// built_in data type: number,string,boolean, void, undefine, null
+
+let userId: number;
+let firstName: string;
+let lastName: string;
+let fullName: string;
+let isActivated: boolean;
+
+
+fullName 
+
+userId = 101;
+firstName = "Sohel";
+lastName = "Rana";
+isActivated = true;
