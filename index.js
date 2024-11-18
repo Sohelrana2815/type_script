5e6bb0b97edcfd75e31e537ab0f2174dd300b2a0
Sohelrana2815 <sohel152302@gmail.com>
2024-11-18 14:40:33 +0600

    learn compile ts file

---
+++ index.js
@@ -0,0 +1,6 @@
+// step 1 : js file k compile korbo
+// step 2 : command tsc index.ts => index.js => node index.js
+var userName = "SOhel Rana";
+var age = 25;
+console.log(userName);
+console.log(age);
---
+++ index.ts
@@ -0,0 +1,10 @@
+// step 1 : js file k compile korbo
+// step 2 : command tsc index.ts => index.js => node index.js
+
+let userName = "SOhel Rana";
+
+let age = 25;
+
+console.log(userName);
+console.log(age);
+
