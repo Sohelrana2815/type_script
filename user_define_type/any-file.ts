03954e2d2733232db2f6a2629bf1d43a0b4a7d33
Sohelrana2815 <sohel152302@gmail.com>
2024-11-20 06:45:31 +0600

    learn about any type

---
+++ user_define_type/any-file.ts
@@ -0,0 +1,12 @@
+// Any type
+// when to use if u create a form then you are not sure what input a user will type inside that form then you can use any type
+
+
+
+let userName: any;
+
+userName = "sohel";
+userName = 123545;
+userName = true;
+
+userName = [1, 2, 3];
