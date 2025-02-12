eddfc9c7360613ff7ea30b54d0c3633a8b44d691
Sohelrana2815 <sohel152302@gmail.com>
2025-02-12 08:41:57 +0600

    learn typescript

---
+++ index1.ts
@@ -0,0 +1,18 @@
+type Person = {
+  name: string;
+  age: number;
+  isStudent: boolean;
+};
+
+let person: Person = {
+  name: "John",
+  age: 45,
+  isStudent: false,
+};
+
+let person2: Person = {
+  name: "Sam",
+  age: 36,
+  isStudent: true,
+};
+
