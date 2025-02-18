3268df4015819eec94d4985a8b909c422be9e37b
Sohelrana2815 <sohel152302@gmail.com>
2025-02-18 07:38:20 +0600

    done

--- copy.ts
+++
@@ -1,9 +0,0 @@
-type User = {
-    username: string;
-    role: "guest" | "member" | "admin";
-  };
-  
-  type UserRole = "guest" | "member" | "admin";
-  
-  let userRole: UserRole = "member";
-  
