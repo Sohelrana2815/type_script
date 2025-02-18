50a59fd62e3518faa4be11e718c7338d81ea10da
Sohelrana2815 <sohel152302@gmail.com>
2025-02-18 08:50:13 +0600

    done

---
+++ Functions/index.js
@@ -0,0 +1,8 @@
+// function helloWorld(personName: string, age: number) {
+//   console.log("hello", personName, age);
+// }
+// helloWorld("world!.", 24);
+function addition(num1, num2) {
+    return num1 + num2;
+}
+console.log(addition(12, 567));
---
+++ Functions/index.ts
@@ -0,0 +1,10 @@
+// function helloWorld(personName: string, age: number) {
+//   console.log("hello", personName, age);
+// }
+
+// helloWorld("world!.", 24);
+
+function addition(num1: number, num2: number): number {
+  return num1 + num2;
+}
+console.log(addition(12, 45));
