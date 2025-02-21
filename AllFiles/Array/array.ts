dfb29dcd2eaf153e616ef8c7228ab9c9ce6be42d
Sohelrana2815 <sohel152302@gmail.com>
2025-02-21 08:08:12 +0600

    done

---
+++ AllFiles/Array/array.ts
@@ -0,0 +1,25 @@
+// let arr: number[] = [];
+
+// arr[0] = 32;
+
+// arr.push(7);
+
+let NameOfTeachers: string[] = ["max", "Jemmy"];
+
+let AgeOfTeachers: number[] = [45, 32];
+
+let Teachers: boolean[] = [false, true];
+
+let towDArr: number[][] = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+let threeDArr: number[][][] = [
+  [
+    [12, 13, 14],
+    [15, 16, 17],
+    [18, 19, 20],
+  ],
+];
