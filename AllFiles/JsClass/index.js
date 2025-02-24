9b2b2b911f03a47060845a3ebd1732b11709d1f9
Sohelrana2815 <sohel152302@gmail.com>
2025-02-24 11:01:10 +0600

    class in typescript

--- AllFiles/JsClass/index.js
+++ AllFiles/JsClass/index.js
@@ -1,53 +1,60 @@
-// class Computer {
-//   constructor(name, ram) {
+// // class Computer {
+// //   constructor(name, ram) {
+// //     this.name = name;
+// //     this.ram = ram;
+// //   }
+
+// //   aboutComputer() {
+// //     console.log(`Computer name: ${this.name}, Ram: ${this.ram} GB`);
+// //   }
+// // }
+
+// // let razer = new Computer("Razer 123", 16);
+
+// // console.log("Razer", razer);
+// // razer.aboutComputer();
+
+// class PersonInfo {
+//   #gender = "male";
+//   height = "5.6ft";
+//   constructor(name, age, isMarried) {
 //     this.name = name;
-//     this.ram = ram;
+//     this.age = age;
+//     this.isMarried = isMarried;
 //   }
 
-//   aboutComputer() {
-//     console.log(`Computer name: ${this.name}, Ram: ${this.ram} GB`);
+//   aboutPerson() {
+//     console.log(
+//       `Person info: Name: ${this.name}, age is: ${this.age}, is he married: ${
+//         this.isMarried
+//       } and gender is: ${this.#gender} height is: ${this.height}`
+//     );
+//     this.#genderCheck();
+//   }
+//   #genderCheck() {
+//     console.log(`Only selected gender is: ${this.#gender}`);
+//   }
+
+//   get heightDetails() {
+//     return this.height;
+//   }
+//   set heightDetails(ft) {
+//     this.height = ft;
 //   }
 // }
 
-// let razer = new Computer("Razer 123", 16);
+// let person1 = new PersonInfo("John_Doe", 35, true);
 
-// console.log("Razer", razer);
-// razer.aboutComputer();
+// console.log(person1);
+// console.log("Get", person1.heightDetails);
+// person1.heightDetails = "06ft";
+// console.log(person1.heightDetails);
 
-class PersonInfo {
-  #gender = "male";
-  height = "5.6ft";
-  constructor(name, age, isMarried) {
+// person1.aboutPerson();
+
+class Computer {
+  constructor(name, ram) {
     this.name = name;
-    this.age = age;
-    this.isMarried = isMarried;
-  }
-
-  aboutPerson() {
-    console.log(
-      `Person info: Name: ${this.name}, age is: ${this.age}, is he married: ${
-        this.isMarried
-      } and gender is: ${this.#gender} height is: ${this.height}`
-    );
-    this.#genderCheck();
-  }
-  #genderCheck() {
-    console.log(`Only selected gender is: ${this.#gender}`);
-  }
-
-  get heightDetails() {
-    return this.height;
-  }
-  set heightDetails(ft) {
-    this.height = ft;
+    this.ram = ram;
   }
 }
-
-let person1 = new PersonInfo("John_Doe", 35, true);
-
-console.log(person1);
-console.log("Get", person1.heightDetails);
-person1.heightDetails = "06ft";
-console.log(person1.heightDetails);
-
-person1.aboutPerson();
---
+++ AllFiles/JsClass/index.ts
@@ -0,0 +1,8 @@
+class Computer {
+  name: string;
+  ram: number;
+  constructor(name: string, ram: number) {
+    this.name = name;
+    this.ram = ram;
+  }
+}
