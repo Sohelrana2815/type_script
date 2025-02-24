03429414b8b57503d410a3392c7d032efa142010
Sohelrana2815 <sohel152302@gmail.com>
2025-02-24 09:02:28 +0600

    done

--- AllFiles/Interfaces/Interfaces.ts
+++ AllFiles/Interfaces/Interfaces.ts
@@ -11,16 +11,10 @@ interface People {
   fullName(): string;
 }
 
-interface Employee {
-  firstName: string;
-  lastName: string;
-  age: number;
-  email: string;
-  fullName(): string;
+interface Employee extends People {
   designation: string;
   yearOfExperience: number;
 }
-
 let sohel: Employee = {
   firstName: "Sohel",
   lastName: "Rana",
@@ -29,8 +23,8 @@ let sohel: Employee = {
   fullName: function () {
     return `${this.firstName} ${this.lastName} age is: ${this.age} email: ${this.email} his designation is ${this.designation} and he has ${this.yearOfExperience}`;
   },
-  designation: "bla bla",
-  yearOfExperience: 2,
+  designation: "Frontend Developer",
+  yearOfExperience: 1.5,
 };
 
 console.log(sohel.fullName());
